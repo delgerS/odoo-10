14402abb3873838cdaec8697abb6c65532e019fe
khyasir <yasirkrauf@yahoo.com>
2017-10-16 17:47:10 +0500

    this is your comment

--- odoo_10_module_Git.py
+++ odoo_10_module_Git.py
@@ -7,9 +7,9 @@ os.system('git remote add origin https://github.com/khyasir/odoo-10.git')
 os.system('git add *')
 os.system('git status')
 os.system('git commit -m "this is your comment"')
-master='git push origin master'
-p = os.system('echo %s| sudo -S %s' % (username, master))
-# os.system('git push origin master')
+# master='git push origin master'
+# p = os.system('echo %s| sudo -S %s' % (username, master))
+os.system('git push origin master')
 # os.system('echo %s' % (master))
 # username='khyasir'
 # password='yasir43'
