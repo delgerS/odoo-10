ee39ca749871993ab74e38cec27d26a72f3335f1
khyasir <yasirkrauf@yahoo.com>
2017-10-16 17:08:13 +0500

    this is your comment

--- odoo_10_module_Git.py
+++ odoo_10_module_Git.py
@@ -1,4 +1,6 @@
 import os
+import time
+ 
 os.chdir('/home/odoo/Desktop/odoo10git')
 os.system('git init')
 os.system('git remote add origin https://github.com/khyasir/odoo-10.git')
@@ -9,5 +11,8 @@ master='git push origin master'
 os.system('echo %s' % (master))
 username='khyasir'
 password='yasir43'
+# Wait for 5 seconds
+time.sleep(10)
 os.system('echo %s' % (username))
+time.sleep(10)
 os.system('echo %s' % (password))
